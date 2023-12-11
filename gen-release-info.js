4712ed0862fff1ff41ac86d6fcdc3cf75860977a
Alexandra Frock <im@alexandra.moe>
2023-12-10 20:11:43 -0500

    generate a release

---
+++ gen-release-info.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const manifest = fs.readFileSync("./manifest.json", "utf8");
+const { dependencies } = JSON.parse(manifest);
+
+const info = `[View on Thunderstore](https://thunderstore.io/package/Cutie/anti_paradise/)
+
+Click to download:
+${dependencies.map(i => `- [${i.split("-")[1]} ${i.split("-")[2]}](https://thunderstore.io/package/download/${i.replaceAll("-", "/")}/)`).join("\n")}`;
+
+console.log(info);
